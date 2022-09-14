8035f23cda3b62ebb4f444c01221b9a83f3dd65a
akshayady <akshayadi1997@gmail.com>
2022-09-14 11:05:25 +0530

    gdfgdd

--- middleware/cloud.js
+++ middleware/cloud.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary')
 
 cloudinary.config({ 
-    cloud_name: 'sid-waycool', 
-    api_key: '313318984626372', 
-    api_secret: 'Yvvcm8s7ZgR7U9RWrIAb_KSpmQI' 
+    cloud_name: 'dwn9amt1o', 
+    api_key: '483137176619167', 
+    api_secret: 'GNu57Ewol1Uq-gPzadoMgjWdF-I' 
   });
 
-  module.exports = cloudinary
+module.exports = cloudinary
